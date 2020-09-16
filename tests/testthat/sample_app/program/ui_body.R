6e04d53d3c460aede858d2214a93c715b926e6b3
Connie Brett <connie@aggregate-genius.com>
2020-09-16 16:31:01 +0000

    cleanup

--- tests/testthat/sample_app/program/ui_body.R
+++ tests/testthat/sample_app/program/ui_body.R
@@ -47,7 +47,7 @@ body3 <- shinydashboard::box( id     = "bodyElement3",
               collapsible = TRUE,
               collapsed   = TRUE,
               downloadableTableUI("exampleDT1",
-                                  list("csv", "tsv", "xlsx"),
+                                  list("csv", "tsv"),
                                   "Download table data") )
 
 body4 <- shinydashboard::box( id = "bodyElement4",
